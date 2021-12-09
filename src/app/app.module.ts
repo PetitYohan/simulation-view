de4cc8696dca26e25d2a16ed0b2724f79054b243
PetitYohan <yohan.petit@cpe.fr>
2021-12-09 15:50:33 +0100

    creation component map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, MapComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
