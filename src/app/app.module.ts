b17eb4f4e8cdead4054092111a755df0714d67df
PetitYohan <yohan.petit@cpe.fr>
2021-12-10 11:59:44 +0100

    Affichage marker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { MarkerService } from './marker.service';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  providers: [MarkerService],
   declarations: [AppComponent, MapComponent],
   bootstrap: [AppComponent],
 })
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,5 +1,21 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import { MarkerService } from '../marker.service';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41],
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-map',
@@ -11,8 +27,8 @@ export class MapComponent implements AfterViewInit {
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [45.7841601, 4.8697515],
-      zoom: 15,
+      center: [39.8282, -98.5795],
+      zoom: 3,
     });
 
     const tiles = L.tileLayer(
@@ -28,9 +44,10 @@ export class MapComponent implements AfterViewInit {
     tiles.addTo(this.map);
   }
 
-  constructor() {}
+  constructor(private markerService: MarkerService) {}
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.markerService.makeCapitalMarkers(this.map);
   }
 }
