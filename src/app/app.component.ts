5ba42ee8614bd0674c6adeb62d07ba1ca3696ac7
PetitYohan <yohan.petit@cpe.fr>
2022-01-03 17:31:05 +0100

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'map',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/icons/map-marked-alt-solid.svg'
+      )
+    );
+    this.matIconRegistry.addSvgIcon(
+      'wrench',
+      this.domSanitizer.bypassSecurityTrustResourceUrl(
+        '../assets/icons/wrench-solid.svg'
+      )
+    );
+  }
 }
