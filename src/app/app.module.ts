2d1a5fb1a514b469d38e8d407069d46ad24edb9d
PetitYohan <yohan.petit@cpe.fr>
2022-01-03 15:34:56 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { MapComponent } from './map/map.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MarkerService } from './marker.service';
 import { PopUpService } from './popup.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, MatIconModule],
   providers: [MarkerService, PopUpService],
   declarations: [AppComponent, MapComponent],
   bootstrap: [AppComponent],
