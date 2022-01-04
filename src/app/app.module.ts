aaaa74bbc06c3d132d4c7b63c93b71216cb78593
PetitYohan <yohan.petit@cpe.fr>
2022-01-04 09:01:36 +0100

    ajout component slider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { SlidersComponent } from './sliders/sliders.component';
 import { HttpClientModule } from '@angular/common/http';
 import { MarkerService } from './marker.service';
 import { PopUpService } from './popup.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
-import { SliderComponent } from '@angular-slider/ngx-slider/slider.component';
 
 @NgModule({
   imports: [
@@ -22,7 +22,7 @@ import { SliderComponent } from '@angular-slider/ngx-slider/slider.component';
     NgxSliderModule,
   ],
   providers: [MarkerService, PopUpService],
-  declarations: [AppComponent, MapComponent, SliderComponent],
+  declarations: [AppComponent, MapComponent, SlidersComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
