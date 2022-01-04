6bb81d40da34b479e155dce111942d9197744679
PetitYohan <yohan.petit@cpe.fr>
2022-01-04 13:31:53 +0100

    2 files modified

--- src/app/sliders/sliders.component.ts
+++ src/app/sliders/sliders.component.ts
@@ -3,12 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sliders',
   templateUrl: './sliders.component.html',
-  styleUrls: ['./sliders.component.css']
+  styleUrls: ['./sliders.component.css'],
 })
 export class SlidersComponent implements OnInit {
-  sliderValue = 0
-  constructor() { }
+  sliderValue = 0;
+  valeurs = [0, 1, 2, 3];
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  readSlider(event) {
+    console.error(event);
   }
 }
