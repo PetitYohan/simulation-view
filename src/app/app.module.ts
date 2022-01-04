817132725c5c185d707d48aef7583408e21e2691
PetitYohan <yohan.petit@cpe.fr>
2022-01-04 09:26:24 +0100

    Ajout ngx module (ne fonctionne pas)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MarkerService } from './marker.service';
 import { PopUpService } from './popup.service';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { NgxSliderModule } from '@angular-slider/ngx-slider';
 
 @NgModule({
   imports: [
@@ -18,6 +19,7 @@ import { MatButtonModule } from '@angular/material/button';
     HttpClientModule,
     MatIconModule,
     MatButtonModule,
+    NgxSliderModule,
   ],
   providers: [MarkerService, PopUpService],
   declarations: [AppComponent, MapComponent, SlidersComponent],
